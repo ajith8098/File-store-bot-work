ea41460c812f5086f9598b00bfcff7bf9b7b0e5d
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2023-07-31 13:50:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6298913586:AAHh99PRj9BarCQViZxaZdIZPlctt0GpHH4")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6489443094:AAGgQQQHfqmH4AGgwBi_d9e--qi0br4AKaQ")
 
 #Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "23990433"))
@@ -32,7 +32,7 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://nakflixbot:alpha3720@clus
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001862282502"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
